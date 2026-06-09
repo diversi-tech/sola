632e0834bb5dc9e68a63c1c94ed1648ae643b5c5
ruti paniri <rp0556797395@gmail.com>
2026-06-09 11:27:48 +0300

    remove_repository

--- authentication_service/src/index.ts
+++ authentication_service/src/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import authRoutes from './routes/auth.routes.js'; 
+import authRoutes from './routes/authentication.routes.js'; 
 
 const app = express();
 const PORT = process.env.PORT;
--- authentication_service/src/routes/authentication.routes.ts
+++ authentication_service/src/routes/authentication.routes.ts
@@ -3,8 +3,4 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.post('/verify-phone', (req: Request, res: Response) => {
-    res.json({ message: "קיבלנו את הבקשה בנתיב ה-auth!" });
-});
-
 export default router;
