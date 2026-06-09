6629f5d7cedde95d6dd50e5fbd83907eba93354a
SHIFI-ZAFRANI <328518618@zzv.org.il>
2026-06-09 10:26:35 +0300

    task_6_Server_Project_Setup_and_Folder_Structure

---
+++ Report_Service/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+ app.use(cors())
+
+
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('השרת פעיל ומאזין לפורט 5003');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
