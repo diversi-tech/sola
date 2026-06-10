e30bc2d17e4db862398b64b1587d1819b19ab6ac
Gili Lev <gili6760558@gmail.com>
2026-06-10 14:11:41 +0300

    Task 8 one page that handles all errors1

--- report-service/src/utils/responseHandler.ts
+++ report-service/src/utils/responseHandler.ts
@@ -1,6 +1,5 @@
 import { Response } from 'express';
 
-
 export const sendSuccess = (res: Response, data: any, statusCode: number = 200) => {
   return res.status(statusCode).json({ success: true, data });
 };
@@ -10,22 +9,18 @@ export const sendError = (res: Response, message: string, statusCode: number = 5
 };
 
 
-// Error - 400
 export const sendBadRequest = (res: Response, message: string = 'נתונים לא תקינים או חסרים') => {
   return sendError(res, message, 400);
 };
 
-// Error - 401
 export const sendUnauthorized = (res: Response, message: string = 'משתמש לא מורשה, יש להתחבר מחדש') => {
   return sendError(res, message, 401);
 };
 
-// Error - 403
 export const sendForbidden = (res: Response, message: string = 'אין לך הרשאות גישה למשאב זה') => {
   return sendError(res, message, 403);
 };
 
-// Error - 404
 export const sendNotFound = (res: Response, message: string = 'המשאב המבוקש לא נמצא') => {
   return sendError(res, message, 404);
 };
