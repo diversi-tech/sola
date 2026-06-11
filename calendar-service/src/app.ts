7dda9e9c00e11ebd9727dfc4b6a1e4050b3489af
Efrat8277 <329172290@zzv.org.il>
2026-06-11 14:28:40 +0300

    fix yo pull request 2

---
+++ calendar-service/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+  res.send('Server is up and running')
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+})
