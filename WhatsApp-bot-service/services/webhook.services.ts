5e2b94f5d7d65413257137787704fd4b204fe970
yaeli-hain <yh21661355@gmail.com>
2026-06-11 11:41:41 +0300

    task_45_extract_phone

--- WhatsApp-bot-service/services/webhook.services.ts
+++ WhatsApp-bot-service/services/webhook.services.ts
@@ -1,12 +1,40 @@
 export const checkVerifyToken = (mode: string, token: string): boolean => {
-    const verifyToken = process.env.VERIFY_TOKEN;
-    return mode === 'subscribe' && token === verifyToken;
+  const verifyToken = process.env.VERIFY_TOKEN;
+  return mode === 'subscribe' && token === verifyToken;
 };
 
 
 export const processWebhookEvent = (body: any) => {
-    //todo:
-    // כאן בהמשך הספרינט אנחנו נוסיף את הקוד שמפרק את ההודעה,
-    // שולח למסד הנתונים של Supabase, או מחזיר תשובה למשתמש.
-    console.log(':envelope_with_arrow: Received webhook event in service:', JSON.stringify(body, null, 2));
-}
+    
+    console.log(':envelope_with_arrow: Received webhook event in service:', JSON.stringify(body, null, 2));
+
+    //check if the webhook event is from a WhatsApp Business Account
+    if (body.object === 'whatsapp_business_account') {
+        
+        // extract the messages array from the webhook payload
+        const messages = body.entry?.[0]?.changes?.[0]?.value?.messages;
+
+        
+        if (messages && messages.length > 0) {
+            const message = messages[0];
+            
+            //extracts sender's phone number from the message object
+            const extractedPhoneNumber = message.from; 
+
+            //validation to check that the extracted phone number is a non-empty string 
+            if (typeof extractedPhoneNumber === 'string' && extractedPhoneNumber.trim() !== '') {
+                
+                
+                let senderPhoneNumber = extractedPhoneNumber; 
+                
+                console.log("✅ Success! Extracted phone number:", senderPhoneNumber);
+                
+                // return the phone number so we can use it later in the flow (e.g., task #46)
+                return senderPhoneNumber;
+                
+            } else {
+                console.error("❌ Validation failed: Phone number is missing or invalid");
+            }
+        }
+    }
+};
