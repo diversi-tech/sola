1c96e992f98862f2efd4c0c5c1cb4e1cd2ddeb69
yv5824 <y0548548713@gmail.com>
2026-06-21 00:40:47 +0300

    Update message.controller.ts

--- WhatsApp-bot-service/controllers/message.controller.ts
+++ WhatsApp-bot-service/controllers/message.controller.ts
@@ -10,24 +10,24 @@ export const handleSendInternalMessage = async (req: Request, res: Response) => 
         return res.status(401).json({ error: "Unauthorized: Invalid API Key" });
     }
 
-    const { toPhoneNumber, success, data, error } = req.body;
+    const { toPhoneNumber: to, success, data, error } = req.body;
 
-    if (!toPhoneNumber) {
-        return res.status(400).json({ error: "Missing required field: toPhoneNumber " });
+    if (!to) {
+        return res.status(400).json({ error: "Missing required field: toPhoneNumber" });
     }
+    
     let textContent = "";
 
-
     if (success === true) {
-    const summary = data?.savedReport?.[0]?.text_summary || "No text summary received";
-    textContent = `Hi! Your report was successfully received and saved. Here is the analysis summary:\n${summary}`;
-   } else {
-    const errorMessage = error || "An unknown error occurred in the system";
-    textContent = `Oops, something went wrong while processing your report:\n${errorMessage}`;
-   }
+        const summary = data?.savedReport?.[0]?.text_summary || "No text summary received";
+        textContent = `Hi! Your report was successfully received and saved. Here is the analysis summary:\n${summary}`;
+    } else {
+        const errorMessage = error || "An unknown error occurred in the system";
+        textContent = `Oops, something went wrong while processing your report:\n${errorMessage}`;
+    }
 
     try {
-        await sendWhatsAppMessage(toPhoneNumber, textContent);
+        await sendWhatsAppMessage(to, textContent);
         
         return res.status(200).json({ 
             success: true, 
