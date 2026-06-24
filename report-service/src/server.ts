2279c2c5d88665405d23bd6ab223c18b2a6f8bae
saralev111 <saralev45@gmail.com>
2026-06-24 12:37:19 +0300

    add and edit categories

--- report-service/src/server.ts
+++ report-service/src/server.ts
@@ -4,12 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import reportRoutes from './routes/report.routes.js';
+import categoryRoutes from './routes/category.routes.js';   
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/reports', reportRoutes);
+
+app.use('/api/categories', categoryRoutes);
 
 const port = process.env.PORT || 5003;
 
