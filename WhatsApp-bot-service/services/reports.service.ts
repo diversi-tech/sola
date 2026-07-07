7c8716154585fdde46e2a66c4ced7601cd1b0380
yaeli-hain <yh21661355@gmail.com>
2026-07-07 15:13:57 +0300

    feat: handle complete reports JSON response and connect translation

--- WhatsApp-bot-service/services/reports.service.ts
+++ WhatsApp-bot-service/services/reports.service.ts
@@ -1,25 +1,24 @@
 import axios from 'axios';
 import { ReportIncomingData } from '../types/reports.types';
 
-export const sendToReports = async (data: ReportIncomingData): Promise<boolean> => {
-    try {
+export const sendToReports = async (data: ReportIncomingData): Promise<any> => {
+        try {
         const reportsApiUrl = process.env.REPORTS_SERVICE_URL; 
         const reportsApiPath = process.env.REPORTS_SERVICE_API_PATH;
         
         if (!reportsApiUrl || !reportsApiPath) {
             console.error("Missing REPORTS_SERVICE_URL or REPORTS_SERVICE_API_PATH in environment variables");
-            return false;
+            return null;
         }
 
         const reportsServiceUrl = `${reportsApiUrl}${reportsApiPath}`;
 
         console.log(`Sending real data to Reports Service at ${reportsServiceUrl}...`);
-        await axios.post(reportsServiceUrl, data);
-        
+        const { data: responseData } = await axios.post(reportsServiceUrl, data);        
         console.log("Data successfully delivered to Reports Team!");
-        return true;
+        return responseData;
     } catch (error) {
         console.error("Failed to send data to Reports Service:", error);
-        return false;
+        return null;
     }
 };
