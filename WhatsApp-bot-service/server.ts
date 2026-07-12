5d7a9bc8cc06a066bdc68258ce9a1f083f8002fa
shira carmi <shira.c215@gmail.com>
2026-07-12 11:55:57 +0300

    deleted API message-corrected

--- WhatsApp-bot-service/server.ts
+++ WhatsApp-bot-service/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webhookRoutes from './routes/webhook.routes';
-//import messageRoutes from './routes/message.routes';
 import dns from 'dns';
 dns.setDefaultResultOrder('ipv4first');
 
@@ -10,7 +9,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-//app.use('/messages', messageRoutes);
 
 const PORT = process.env.PORT || 5001;
 
