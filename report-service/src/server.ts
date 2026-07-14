bff778426d465f53f37080ceb75302adf37d4593
saralev111 <saralev45@gmail.com>
2026-07-14 12:39:37 +0300

    deleted employee page

--- report-service/src/server.ts
+++ report-service/src/server.ts
@@ -8,7 +8,7 @@ import categoryRoutes from './routes/category.routes.js';
 
 const app = express();
 app.use(cors({ 
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173' 
+  origin: process.env.FRONTEND_URL
 }));
 app.use(express.json());
 
