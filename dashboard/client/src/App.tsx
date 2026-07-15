c6e87b19f6237a019ea2273f5128d87ba4a2b9aa
Tamar-Zwi <tamarzwiebel@gmail.com>
2026-07-15 12:17:21 +0300

    add app

--- dashboard/client/src/App.tsx
+++ dashboard/client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { LoginPage } from './pages/LoginPage';
 import EmployeePage from './pages/EmployeesPage';
 import AdminPage from './pages/AdminPage';
-import './index.css';
+import  './index.css';
 function App() {
   return (
     <Router>
@@ -10,7 +10,7 @@ function App() {
         <Route path="/EmployeePage" element={<EmployeePage />} />
         <Route path="/admin" element={<AdminPage/>} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Navigate to="/admin" />} />
+        <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
