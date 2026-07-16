100e203e0f15a5824c5f43075ffe573c85a7cdd0
saralev111 <saralev45@gmail.com>
2026-07-16 12:19:22 +0300

    delete console.log keys

--- report-service/src/config/supabase.ts
+++ report-service/src/config/supabase.ts
@@ -3,9 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log("DEBUG - URL:", process.env.SUPABASE_URL);
-console.log("DEBUG - KEY:", process.env.SUPABASE_ANON_KEY);
-
 export const supabase = createClient(
   process.env.SUPABASE_URL || '',
   process.env.SUPABASE_ANON_KEY || ''
